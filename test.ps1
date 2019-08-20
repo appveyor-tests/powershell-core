cb3a3b8392c70582c59afe593b2aa50b8aa9a478
Alena Hrynkevich <alena@appveyor.com>
2019-08-20 11:55:46 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
-write-host Hello
-$PSVersionTable.PSVersion.Major
+
+echo $PSVersionTable.PSVersion.Major
 if ($PSVersionTable.PSVersion.Major -le 5) {throw "PowerShell version is less or equel than 5. Should be at least 6 when called from -pwsh prefix"}
