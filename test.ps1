7dc88dc2de1523bb1cffd3e9838f3cdeb451f598
Alena Hrynkevich <alena@appveyor.com>
2019-08-23 11:11:27 -0700

    Delete test.ps1

--- test.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "When called from script PSVersionTable.PSVersion.Major is $($PSVersionTable.PSVersion.Major)"
-# $($PSVersionTable.PSVersion)
-if ($PSVersionTable.PSVersion.Major -le 5) {throw "PowerShell version is less or equel than 5. Should be at least 6 when called from -pwsh prefix"}
